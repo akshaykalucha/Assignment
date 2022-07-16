8efabee66d187dc1af6061e734465cde2d013169
akshaykalucha3 <akshaykalucha@gmail.com>
2022-07-16 20:48:57 +0530

    App initialised

---
+++ Assignment/Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by Akshay Kalucha on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment/Assignment/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  Assignment
+//
+//  Created by Akshay Kalucha on 16/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
