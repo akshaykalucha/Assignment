03c69b985f878279ddf6ca451f30f588f69199f9
akshaykalucha3 <akshaykalucha@gmail.com>
2022-07-16 21:23:33 +0530

    news article model instantiated

---
+++ Assignment/Assignment/App/Features/Models/NewsModel.swift
@@ -0,0 +1,29 @@
+//
+//  NewsModel.swift
+//  Assignment
+//
+//  Created by Akshay Kalucha on 16/07/22.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct NewsModel: Codable, Hashable, Identifiable {
+    let id = UUID() // default ID for each object of this model, this will help us solve UI rendering issues
+    let totalResults: Int?
+    let articles: [NewsArticles]?
+}
+
+
+struct NewsArticles: Codable, Hashable, Identifiable {
+    let id = UUID()
+    let source: ArticleSource
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+    let content: String?
+}
